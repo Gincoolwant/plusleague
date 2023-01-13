2bba8fbc906021d7b7235db47e2cb4b54d454655
Gincoolwant <soulbox790326@gmail.com>
2023-01-13 11:28:22 +0800

    feat: incert event to google calendar

--- app.js
+++ app.js
@@ -4,10 +4,71 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const handlebarsHelpers = require('./helpers/handlebars-helper')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
+
+const { google } = require('googleapis')
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URL
+)
+const scopes = [
+  'https://www.googleapis.com/auth/calendar'
+]
+const calendar = google.calendar({ version: 'v3', auth: process.env.GOOGLE_API_KEY })
+
+app.get('/google', (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes
+  })
+
+  res.redirect(url)
+})
+
+app.get('/google/redirect', async (req, res) => {
+  const { tokens } = await oauth2Client.getToken(req.query.code)
+  oauth2Client.setCredentials(tokens)
+
+  res.redirect('/')
+})
+
+app.get('/google/schedule/', (req, res) => {
+  const event = {
+    summary: 'Test event',
+    description: 'Google add event testing.',
+    start: {
+      dateTime: '2023-01-11T01:00:00-07:00',
+      timeZone: 'Asia/Kolkata'
+    },
+    end: {
+      dateTime: '2023-01-11T05:00:00-07:00',
+      timeZone: 'Asia/Kolkata'
+    },
+    reminders: {
+      useDefault: false,
+      overrides: [
+        { method: 'email', minutes: 24 * 60 },
+        { method: 'popup', minutes: 30 }
+      ]
+    }
+  }
+
+  calendar.events.insert({
+    auth: oauth2Client,
+    calendarId: 'primary',
+    resource: event
+  })
+    .then(event => console.log(event.data.htmlLink))
+    .catch((error) => console.log('Some error occured', error))
+})
 
 app.use('/', async (req, res) => {
   axios('https://pleagueofficial.com/schedule-regular-season/2022-23')
