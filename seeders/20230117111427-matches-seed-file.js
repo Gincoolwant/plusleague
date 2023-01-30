78f78fe67ebb8cdd90e03fb96584dfe552912b8a
Gincoolwant <soulbox790326@gmail.com>
2023-01-31 01:52:11 +0800

    model: add teamId

--- seeders/20230117111427-matches-seed-file.js
+++ seeders/20230117111427-matches-seed-file.js
@@ -1,8 +1,18 @@
 'use strict'
-const matches = require('../regular-season.json')
+let matches = require('../regular-season.json')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const teams = await queryInterface.sequelize.query(
+      'SELECT id, team_id FROM Teams;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT })
+    matches = matches.map(match => ({
+      game_id: match.game_id,
+      game_time: match.game_time,
+      arena: match.arena,
+      guest_id: teams[match.guest_id - 1].id,
+      home_id: teams[match.home_id - 1].id
+    }))
     await queryInterface.bulkInsert('Matches', matches, {})
   },
   async down (queryInterface, Sequelize) {
