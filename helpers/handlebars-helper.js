35bbbd07fefd07f872ec841280aeda96974a8af4
Gincoolwant <soulbox790326@gmail.com>
2023-02-01 11:17:03 +0800

    feat: given tz setting to taipei

--- helpers/handlebars-helper.js
+++ helpers/handlebars-helper.js
@@ -1,13 +1,17 @@
 const dayjs = require('dayjs')
 require('dayjs/locale/zh-cn')
 const relativeTime = require('dayjs/plugin/relativeTime')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
 dayjs.extend(relativeTime)
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 module.exports = {
   currentYear: () => dayjs().year(),
-  parseDate: (time) => dayjs(time).format('MM/DD'),
-  parseDay: (time) => dayjs(time).locale('zh-cn').format('dd'),
-  parseTime: (time) => dayjs(time).format('HH:mm'),
+  parseDate: (time) => dayjs(time).tz('Asia/Taipei').format('MM/DD'),
+  parseDay: (time) => dayjs(time).tz('Asia/Taipei').locale('zh-cn').format('dd'),
+  parseTime: (time) => dayjs(time).tz('Asia/Taipei').format('HH:mm'),
   relativeTimeFromNow: time => dayjs(time).fromNow(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
