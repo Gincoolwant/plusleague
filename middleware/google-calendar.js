b3e1d518da7b148fa26a9b117d7f74b74b51ca8f
Gincoolwant <soulbox790326@gmail.com>
2023-02-14 14:57:42 +0800

    fix: set credential with access token &  refresh token

--- middleware/google-calendar.js
+++ middleware/google-calendar.js
@@ -5,6 +5,10 @@ const scopes = [
   'https://www.googleapis.com/auth/calendar'
 ]
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
@@ -15,7 +19,10 @@ const checkOauth = async (req, res, next) => {
   if (req.user.gToken) {
     const gToken = jwt.verify(req.user.gToken, process.env.GOOGLE_CLIENT_SECRET)
     delete gToken.iat
-    oauth2Client.setCredentials(gToken)
+    oauth2Client.setCredentials({
+      access_token: gToken.access_token,
+      refresh_token: gToken.refresh_token
+    })
     return next()
   }
   const data = {
