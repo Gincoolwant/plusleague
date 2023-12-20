d55d7aa0522f04f487c4224e2b42989651453e78
Gincoolwant <soulbox790326@gmail.com>
2023-12-20 13:23:35 +0800

    fix: travis ci database connection

--- config/config.js
+++ config/config.js
@@ -26,12 +26,5 @@ module.exports = {
   },
   production: {
     use_env_variable: 'JAWSDB_MARIA_URL'
-  },
-  travis: {
-    username: 'travis',
-    database: 'plus_league_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    logging: false
   }
 }
