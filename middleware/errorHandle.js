c05880f295c10c8162c1e7ac9fd44f9229ea8f7b
Gincoolwant <soulbox790326@gmail.com>
2024-01-17 16:20:09 +0800

    chore: modify test script

--- middleware/errorHandle.js
+++ middleware/errorHandle.js
@@ -1,11 +1,13 @@
 module.exports = {
   errorHandler (err, res) {
     if (err instanceof Error) {
+      console.log(err.message)
       res.status(err.status || 500).json({
         status: 'error',
         message: '伺服器發生錯誤，請稍後重試或聯絡開發人員。'
       })
     } else {
+      console.log(err.message)
       res.status(501).json({
         status: 'error',
         message: '未預期的錯誤'
