67882d1472c896e88a866dbc3aaf60c962486b14
Gincoolwant <soulbox790326@gmail.com>
2024-01-19 13:15:34 +0800

    feat: add easl schedule

---
+++ seeders/20240119041624-easl23-24-seed-file.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+dayjs.extend(utc)
+
+let matches = require('../crawler/schedule/easl23-24.json')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const teams = await queryInterface.sequelize.query(
+      'SELECT id, team_id FROM Teams;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT })
+    matches = matches.map(match => ({
+      type: match.type,
+      game_id: match.game_id,
+      game_time: dayjs(match.game_time).utc().format('YYYY-MM-DD HH:mm:ss'),
+      arena: match.arena,
+      guest_id: teams[match.guest_id - 1].id,
+      home_id: teams[match.home_id - 1].id
+    }))
+    await queryInterface.bulkInsert('Matches', matches, {})
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Matches', {})
+  }
+}
