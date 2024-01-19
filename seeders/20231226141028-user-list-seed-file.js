04e354e564508063aa6c33adb139cd5d8be36bb3
Gincoolwant <soulbox790326@gmail.com>
2024-01-19 13:13:52 +0800

    fix: store user hash password

--- seeders/20231226141028-user-list-seed-file.js
+++ seeders/20231226141028-user-list-seed-file.js
@@ -1,13 +1,15 @@
 'use strict'
-
 /** @type {import('sequelize-cli').Migration} */
+const bcrypt = require('bcryptjs')
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const salt = bcrypt.genSaltSync(10)
+    const defaultHash = bcrypt.hashSync('123', salt)
     const userList = [
       {
         name: '使用者初代目',
         email: 'user1@example.com',
-        password: '123',
+        password: defaultHash,
         is_admin: 'user',
         g_token: null,
         created_at: new Date(),
@@ -16,7 +18,7 @@ module.exports = {
       {
         name: '管理者1號',
         email: 'admin@example.com',
-        password: '123',
+        password: defaultHash,
         is_admin: 'admin',
         g_token: null,
         created_at: new Date(),
