94cc3b67df0f2b6510a3774e2941a7aaec16beaa
Gincoolwant <soulbox790326@gmail.com>
2024-01-23 19:51:34 +0800

    fix: modify easl seeder

--- seeders/20240119041624-easl23-24-seed-file.js
+++ seeders/20240119041624-easl23-24-seed-file.js
@@ -2,12 +2,8 @@
 
 const dayjs = require('dayjs')
 const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-
 dayjs.extend(utc)
-dayjs.extend(timezone)
 
-dayjs.tz.setDefault('Asia/Taipei')
 let matches = require('../crawler/schedule/2023easl-regular.json')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -19,7 +15,7 @@ module.exports = {
       season: match.season,
       type: match.type,
       game_id: match.game_id,
-      game_time: dayjs.tz(match.game_time).utc().format('YYYY-MM-DD HH:mm:ss'),
+      game_time: dayjs(match.game_time).utc().format('YYYY-MM-DD HH:mm:ss'),
       arena: match.arena,
       guest_id: teams[match.guest_id - 1].id,
       home_id: teams[match.home_id - 1].id
