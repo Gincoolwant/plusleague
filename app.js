3964649257183183ea6690f9973a19e0496b3433
Gincoolwant <soulbox790326@gmail.com>
2024-01-29 20:53:42 +0800

    feat: show 404 page while not found url

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const path = require('path')
 const flash = require('connect-flash')
-const usePassport = require('./config/passport')
 const session = require('express-session')
-const handlebarsHelpers = require('./helpers/handlebars-helper')
-const routes = require('./routes/index')
 const methodOverride = require('method-override')
 const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+
+const usePassport = require('./config/passport')
+const routes = require('./routes/index')
+const handlebarsHelpers = require('./helpers/handlebars-helper')
+const { errorHandler } = require('./middleware/errorHandle')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const app = express()
+const staticFilesPath = path.join(__dirname, '/public')
 
+app.use(morgan('short'))
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
+app.use(express.static(staticFilesPath))
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }))
 app.use(flash())
@@ -34,6 +41,7 @@ app.use((req, res, next) => {
 })
 
 app.use(routes)
+app.use(errorHandler)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`App is listening on port ${port}!`))
